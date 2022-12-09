00c9c4fd8b3e108a05458005c60996d5cd39781e
Julius Steen <steen@cl.uni-heidelberg.de>
2022-12-09 01:59:15 +0100

    Initial commit

---
+++ metabaseline/data.py
@@ -0,0 +1,94 @@
+import datasets
+from datasets.arrow_dataset import Dataset
+from transformers.tokenization_utils import PreTrainedTokenizer
+
+import numpy as np
+
+
+# Common format: start_char, end_char, text, label
+
+def load_dataset(path):
+    instances = datasets.load_dataset("json", data_files=path)
+
+    return instances["train"]
+
+from dataclasses import dataclass
+
+@dataclass
+class MetaphorDatasetStats:
+    total_instances: int
+    metaphor_instances: int
+    literal_instances: int
+    total_sentences: int
+    metaphoric_sentences: int
+
+    metaphors_in_metaphoric_sentences: int
+
+    @classmethod
+    def from_dataset(cls, dataset: Dataset):
+        total_instances = 0
+        metaphor_instances = 0
+        literal_instances = 0
+
+        total_sentences = len(dataset)
+
+        metaphoric_sentences = 0
+
+        metaphors_in_metaphoric_sentences = 0
+
+        for entry in dataset:
+            if any(i["label"] == 0 for i in entry["instances"]):
+                metaphors_in_metaphoric_sentences += len(entry["instances"])
+                metaphoric_sentences += 1
+
+            for instance in entry["instances"]:
+                if instance["label"] == 0:
+                    literal_instances += 1
+                else:
+                    metaphor_instances += 1
+            total_instances += len(entry["instances"])
+
+        return cls(
+            total_instances=total_instances,
+            metaphor_instances=metaphor_instances,
+            literal_instances=literal_instances,
+            total_sentences=total_sentences,
+            metaphoric_sentences=metaphoric_sentences,
+            metaphors_in_metaphoric_sentences=metaphors_in_metaphoric_sentences
+        )
+
+    def __str__(self):
+        return f"""
+Num. Tokens:    {self.total_instances}
+%Met:           {(self.metaphor_instances / self.total_instances) * 100:.1f}
+Num. Sents:     {self.total_sentences}
+%Met Sents:     {self.metaphoric_sentences / self.total_sentences:.1f}
+%Met per MetS.: {self.metaphors_in_metaphoric_sentences / self.metaphoric_sentences:.1f}
+        """.strip()
+
+
+def load_and_process_dataset(path, tokenizer):
+    ds = load_dataset(path)
+
+    stats = MetaphorDatasetStats.from_dataset(ds)
+    print(stats)
+
+    return preprocess_dataset(ds, tokenizer)
+
+def preprocess_dataset(dataset: Dataset, tokenizer: PreTrainedTokenizer):
+    def process(elem):
+        tokenized = tokenizer(elem["sentence"], padding="max_length", truncation=True, return_tensors="np")
+        labels = np.full(tokenized.input_ids.shape, -100, dtype=np.int32)
+        #labels[tokenized.attention_mask != 0] = -100
+
+        for instance in elem["instances"]:
+            labels[0, tokenized.char_to_token(instance["char_range"][0])] = instance["label"]
+
+        return {"input_ids": tokenized.input_ids[0], "attention_mask": tokenized.attention_mask[0], "labels": labels}
+
+    ds = dataset.map(
+        process,
+        remove_columns=dataset.column_names
+    )
+
+    return ds
---
+++ metabaseline/train_seq_label.py
@@ -0,0 +1,60 @@
+import transformers
+import numpy as np
+from sklearn.metrics import f1_score
+
+from .data import load_and_process_dataset
+
+
+def main():
+    tokenizer = transformers.AutoTokenizer.from_pretrained("bert-base-uncased")
+    train_dataset = load_and_process_dataset("data/all_pos_train", tokenizer=tokenizer)
+    test_dataset = load_and_process_dataset("data/all_pos_test", tokenizer=tokenizer)
+
+    model = transformers.AutoModelForTokenClassification.from_pretrained("bert-base-uncased", num_labels=2)
+    lr = 5e-5
+    training_args = transformers.TrainingArguments(
+        output_dir=f"met_train",
+        num_train_epochs=3,
+        learning_rate=lr,
+        per_device_train_batch_size=8,   
+        gradient_accumulation_steps=2,
+        per_device_eval_batch_size=64,
+        warmup_ratio=0.06,
+        weight_decay=0.01,
+        fp16=True,
+        report_to="wandb" if False else "none",
+        save_strategy="epoch",
+        save_steps=1000,
+        logging_strategy="steps",
+        logging_steps=10,
+        evaluation_strategy="epoch",
+        eval_delay=100,
+
+    )
+
+    def compute_metrics(eval_results):
+        predicted = np.array(eval_results.predictions).argmax(axis=-1)
+        gold = np.array(eval_results.label_ids)[:,0,:]
+
+        predicted = predicted[gold != -100]
+        gold = gold[gold != -100]
+
+        return {"accuracy": (gold == predicted).mean(), "f1": f1_score(gold, predicted)}
+
+    trainer = transformers.Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=None,
+        compute_metrics=compute_metrics,
+
+        #data_collator=collator,
+    )
+
+    trainer.train()
+
+    results = trainer.predict(test_dataset)
+    print(results.metrics)
+
+if __name__ == "__main__":
+    main()
